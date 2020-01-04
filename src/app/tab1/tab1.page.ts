bc88a380ac8d5f9644bb850e688f2320d2e789d3
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2020-01-04 14:13:45 +0500

    init project

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  value1: number;
+  percentage: number;
+  value2: number;
+  value21: number;
+  percentage2: number;
+  value22: number;
+  constructor() {}
+
+  calculateOnChange(curPoint){
+    switch(curPoint){
+      case '0':
+      case '1':
+        this.value2 = parseFloat((this.value1/(1-(this.percentage/100))).toFixed(2));
+        break;
+      case '2':
+        let a =  ((this.value1/this.value2));
+        this.percentage = parseFloat(((1-a)*100).toFixed(4));
+        break;
+        
+    }
+  }
+
+  calculateOnChange2(curPoint){
+    switch(curPoint){
+      case '0':
+      case '1':
+        this.value22 = parseFloat((this.value21 + this.value21*(this.percentage2/100)).toFixed(2));
+        break;
+      case '2':
+        let a =  ((this.value22- this.value21)/this.value21);
+        this.percentage2 = parseFloat(((a)*100).toFixed(6));
+        
+        break;
+        
+    }
+  }
+
+}
